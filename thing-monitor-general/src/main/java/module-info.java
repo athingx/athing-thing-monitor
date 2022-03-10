69cf675b34b86096464e6c3fb81c5563aac9afc3
vlinux <oldmanpushcart@gmail.com>
2022-03-11 01:36:31 +0800

    修正SPI

--- thing-monitor-general/src/main/java/module-info.java
+++ thing-monitor-general/src/main/java/module-info.java
@@ -3,7 +3,7 @@ module io.github.athingx.athing.thing.monitor.aliyun {
     requires io.github.athingx.athing.thing.monitor;
     requires io.github.athingx.athing.standard.thing;
     requires com.github.oshi;
-    requires static org.slf4j;
+    requires org.slf4j;
 
     exports io.github.athingx.athing.thing.monitor.general;
 
