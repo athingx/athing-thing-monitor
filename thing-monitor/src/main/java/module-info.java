c33283bb5788c8650e6ab7bd1de02d1bd2eb6604
vlinux <oldmanpushcart@gmail.com>
2022-06-05 20:45:28 +0800

    优化monitor-api的包引入策略

--- thing-monitor/src/main/java/module-info.java
+++ thing-monitor/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 open module io.github.athingx.athing.thing.monitor {
-    requires io.github.athingx.athing.standard.thing;
+    requires io.github.athingx.athing.standard.component;
     exports io.github.athingx.athing.thing.monitor.info;
     exports io.github.athingx.athing.thing.monitor.usage;
 }
